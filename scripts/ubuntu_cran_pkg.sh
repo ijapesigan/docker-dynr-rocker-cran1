4f0261b253dbee2a20c51f96e1c1a7b56c02c601
ijapesigan <137515526+ijapesigan@users.noreply.github.com>
2025-08-24 17:05:23 -0400

    Automated build.

--- scripts/ubuntu_cran_pkg.sh
+++ scripts/ubuntu_cran_pkg.sh
@@ -3,18 +3,21 @@
 set -e
 
 install2.r --error --skipinstalled -n -1 \
-  car            \
-  deSolve        \
-  devtools       \
-  fda            \
-  fds            \
-  ggplot2        \
-  knitr          \
-  languageserver \
-  latex2exp      \
-  magrittr       \
-  MASS           \
-  Matrix         \
-  mice           \
-  numDeriv       \
-  plyr
+  ggplot2
+
+#install2.r --error --skipinstalled -n -1 \
+#  car            \
+#  deSolve        \
+#  devtools       \
+#  fda            \
+#  fds            \
+#  ggplot2        \
+#  knitr          \
+#  languageserver \
+#  latex2exp      \
+#  magrittr       \
+#  MASS           \
+#  Matrix         \
+#  mice           \
+#  numDeriv       \
+#  plyr
